7a652f0c4b59218129d557ce4c0b91c418947134
Suraj Kumar Verma <surajkumarverma662@gmail.com>
2024-03-21 23:15:36 +0530

    song added

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("lets write javaScript");
+
+async function mainsong(){
+   let a = await fetch("http://127.0.0.1:5500/songs/")
+   let response = await a.text();
+   console.log(response) 
+}
+
+mainsong()
